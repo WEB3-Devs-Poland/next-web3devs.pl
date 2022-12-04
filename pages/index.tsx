ced759a55d9f11998b2c82afd80d9721b9913f68
Kacper Adler <dev@web3devs.pl>
2022-12-04 20:42:03 +0100

    feat: Test font awesome icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+import { faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold">Welcome in Web3 Devs Poland page</h1>;
+  return (
+    <h1 className="text-3xl font-bold">
+      <FontAwesomeIcon icon={faDiscord} />
+      Welcome in Web3 Devs Poland
+    </h1>
+  );
 };
 
 export default Home;
