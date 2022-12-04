f49e3990fd421105884abd7c7627e1595c3695d7
Kacper Adler <dev@web3devs.pl>
2022-12-04 20:36:50 +0100

    feat: Add font awesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import { AppProps } from 'next/app';
 import 'styles/index.css';
+
+config.autoAddCss = false;
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
