3047db212efdace0d015e9b040abad3dea15fa21
Kacper Adler <dev@web3devs.pl>
2022-12-29 20:22:23 +0100

    feat: Add mobile menu

--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -2,20 +2,23 @@ import { LINKS } from './Header.constants';
 import { ROUTE } from '@config/routes';
 import WEB3DevsPolandLogo from '@images/icons/WEB3DevsPolandLogo.svg';
 import Link from 'next/link';
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { Button } from '@components/Button';
+import { Menu, Transition } from '@headlessui/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export const Header: React.FC = () => (
-  <header className="w-full flex justify-around top-0 z-50 m-auto">
-    <div className="w-10/12 flex flex-row justify-between">
+  <header className="top-0 z-50 m-auto flex w-full justify-around">
+    <div className="flex w-10/12 flex-row justify-between">
       <div className="w-36">
         <Link href={ROUTE.home}>
-          <WEB3DevsPolandLogo />
+          <WEB3DevsPolandLogo className="scale-75 lg:scale-100" />
         </Link>
       </div>
 
-      <div className="my-auto">
+      <div className="my-auto hidden lg:block">
         {LINKS.map(({ label, route }) => (
           <Button type="Link" key={label} href={route}>
             {label}
@@ -23,8 +26,40 @@ export const Header: React.FC = () => (
         ))}
       </div>
 
-      <div className="my-auto w-36">
+      <div className="my-auto hidden w-36 lg:block">
         <Button>Connect Wallet</Button>
+      </div>
+
+      <div className="my-auto block lg:hidden">
+        <Menu>
+          <Menu.Button>
+            <FontAwesomeIcon icon={faBars} color="white" size="2x" />
+          </Menu.Button>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95">
+            <Menu.Items className="absolute left-0 right-0 mx-auto my-12 w-10/12 rounded-lg border-2 border-brand-purple2 bg-black p-2 drop-shadow-glow">
+              {LINKS.map(({ label, route }) => (
+                <div
+                  key={label}
+                  className="w-full border-b-[1px] border-brand-purple2 p-6 text-center text-xl last:border-b-0">
+                  <Button type="Link" href={route}>
+                    {label}
+                  </Button>
+                </div>
+              ))}
+
+              <div className="w-full border-b-[1px] border-brand-purple2 p-6 text-center text-xl last:border-b-0">
+                <Button>Connect Wallet</Button>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
       </div>
     </div>
   </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,7 @@ module.exports = {
     extend: {
       dropShadow: {
         card: '0px 0px 16px #9404FC',
+        glow: '0px 0px 20px rgba(255, 255, 255, 0.2)',
       },
     },
   },
