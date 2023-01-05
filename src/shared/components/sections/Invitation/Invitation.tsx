3aaf8aadac3e9981445d66ef97cb4fe741d63502
Kacper Adler <dev@web3devs.pl>
2023-01-05 17:14:01 +0100

    feat: Bigger font and add mobile

--- src/shared/components/sections/Invitation/Invitation.tsx
+++ src/shared/components/sections/Invitation/Invitation.tsx
@@ -1,19 +1,20 @@
 import { Button } from '@components/Button';
+import { MEDIA } from '@components/Footer/Footer.constants';
 import { Section } from '@components/Section';
 import React from 'react';
 
-export const Invitation: React.FunctionComponent = () => {
-  return (
-    <Section className="flex flex-col justify-center">
-      <h1 className="mx-auto max-w-lg text-center text-5xl font-semibold tracking-wide">
-        <p className="inline-block text-brand-purple2">Web3</p> Developers Polish Community
-      </h1>
+export const Invitation: React.FunctionComponent = () => (
+  <Section className="flex flex-col justify-center">
+    <h1 className="mx-auto max-w-xl text-center text-4xl font-semibold tracking-wide lg:text-6xl">
+      <p className="inline-block text-brand-purple2">Web3</p> Developers Polish Community
+    </h1>
 
-      <h2 className="mx-auto my-8 text-center text-2xl font-medium">
-        Learn. <p className="inline-block text-brand-purple2">BUIDL.</p> Grow.
-      </h2>
+    <h2 className="mx-auto my-8 text-center text-xl font-medium lg:text-3xl">
+      Learn. <p className="inline-block text-brand-purple2">BUIDL.</p> Grow.
+    </h2>
 
-      <Button className="m-auto py-2 px-12 text-xl font-semibold">Join us</Button>
-    </Section>
-  );
-};
+    <a href={MEDIA[1].link} className="m-auto">
+      <Button className="py-3 px-14 text-xl font-semibold lg:text-2xl">Join us</Button>
+    </a>
+  </Section>
+);
