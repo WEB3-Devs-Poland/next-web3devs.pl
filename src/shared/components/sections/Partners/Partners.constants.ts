7ca0accdc391b6e11bf6f46eb52e275869324b5c
Kacper Adler <dev@web3devs.pl>
2023-01-07 17:21:42 +0100

    feat: Add v1 partners boxes

---
+++ src/shared/components/sections/Partners/Partners.constants.ts
@@ -0,0 +1,27 @@
+import AlephZero from '@images/partners/alephZero.svg';
+import Hashup from '@images/partners/hashup.svg';
+
+export const PARTNERS = [
+  {
+    name: 'ALEPH ZERO',
+    Logo: AlephZero,
+    description: '',
+    media: [
+      {
+        icon: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    name: 'HASHUP',
+    Logo: Hashup,
+    description: '',
+    media: [
+      {
+        icon: '',
+        link: '',
+      },
+    ],
+  },
+];
--- src/shared/components/sections/Partners/Partners.tsx
+++ src/shared/components/sections/Partners/Partners.tsx
@@ -1,6 +1,18 @@
 import { Section } from '@components/Section';
 import React from 'react';
+import { PARTNERS } from './Partners.constants';
 
 export const Partners: React.FunctionComponent = () => {
-  return <Section title="Partners"></Section>;
+  return (
+    <Section title="Partners" className="flex flex-row justify-center">
+      {PARTNERS.map(({ name, Logo }) => (
+        <div
+          key={name}
+          // eslint-disable-next-line max-len
+          className="m-8 flex aspect-square cursor-pointer rounded-md border-2 border-grayScale-grey2 bg-black p-8 duration-500 ease-in-out hover:border-brand-purple2 hover:drop-shadow-card">
+          <Logo className="self-center" />
+        </div>
+      ))}
+    </Section>
+  );
 };
